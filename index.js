1b688a90599557c5e35f393559ee932cd84f7578
HeadMad <kov.ps@ya.ru>
2020-02-11 13:25:02 +0800

    ready to exploit

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 let postcss = require('postcss')
 
-module.exports = postcss.plugin('PLUGIN_NAME', (opts = { }) => {
+module.exports = postcss.plugin('postcss-cell', (decl = /^(width|height)$/) => {
+  const isCell = /^\d+\/\d+/
+  return (root) => {
+    root.walkDecls(decl, decl => {
+      if (!isCell.test(decl.value)) return
 
-  // Work with options here
+      let matches = decl.value.match(/((\d+)\/(\d+))(\s+((\d+)(px|%|em|rem|pt|wh|wv)))?/)
 
-  return (root, result) => {
+      let width = matches[2]
+      let cols = matches[3]
+      let gap = matches[6]
+      let gaps = gap * (cols - 1)
+      let gapExt = matches[7]
 
+      let value = !gaps ? `${(width / cols) * 100}%`
+        : gapExt === '%' ? (100 * width - gap * (cols - width)) / cols + '%'
+        : `calc(${100 * width / cols}% - ${gap * (cols - width) / cols}${gapExt})`
+
+      decl.cloneBefore({ value })
+      decl.remove()
+    })
     // Transform CSS AST here
 
   }
